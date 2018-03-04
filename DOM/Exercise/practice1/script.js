238a911d6928e077a998c694363167996ece511f
unknown <savudin92vb@gmail.com>
2018-03-04 19:55:43 +0100

    finished

--- DOM/Exercise/practice1/script.js
+++ DOM/Exercise/practice1/script.js
@@ -1,11 +1,11 @@
 var bg = document.querySelector(".wrap");
 var mario = document.querySelector("#mario");
 var marioRun = document.querySelector("#marioRun");
-var bgImg = document.querySelector("#bgImg");
+
 var left = 0;
 var right = 0;
-
-
+var run;
+bg.style.backgroundPositionX = "100px";
 
 document.onkeydown = checkKey;
 
@@ -13,23 +13,25 @@ function checkKey(e) {
 
 	e = e || window.event;
 
-	if (e.keyCode == '38') {
-		// mario.style.display = "none";
-		// marioRun.style.display = "block";
-		// alert("gore");
-	} else if (e.keyCode == '40') {
-		// mario.style.display = "block";
-		// marioRun.style.display = "none";
-	} else if (e.keyCode == '39') {
+	if (e.keyCode == '39') {
 		mario.style.display = "none";
 		marioRun.style.display = "block";
-		left -= 2;
-		bgImg.style.left = left + "px";
+		run = setInterval(function () {
+			bg.style.backgroundPositionX = (parseInt(bg.style.backgroundPositionX) - 10) + 'px';
+		}, 200);
 
+	} else if (e.keyCode == '27') {
+		mario.style.display = "block";
+		marioRun.style.display = "none";
+		marioJump.style.display = "none";
+		clearInterval(run);
 	}
-	// else if (e.keyCode == '39') {
-	// 	right += 2;
-	// 	bgImg.style.left = right + "px";
-	// }
-
-}
+};
+//  else if (e.keyCode == '40') {
+// 	// mario.style.display = "block";
+// 	// marioRun.style.display = "none";
+// } else
+// else if (e.keyCode == '39') {
+// 	right += 2;
+// 	bgImg.style.left = right + "px";
+// }
